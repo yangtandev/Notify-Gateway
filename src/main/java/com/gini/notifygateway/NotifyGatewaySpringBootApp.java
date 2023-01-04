8e338d56569b9d8d5f7a8d4ac02f28bd3c5f9540
Yang Tan <yang.tan@gini.tw>
2023-01-04 15:08:34 +0800

    【Feat】Set mailList as a global variable and update it hourly.

--- src/main/java/com/gini/notifygateway/NotifyGatewaySpringBootApp.java
+++ src/main/java/com/gini/notifygateway/NotifyGatewaySpringBootApp.java
@@ -1,13 +1,17 @@
 package com.gini.notifygateway;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
@@ -17,7 +21,6 @@ public class NotifyGatewaySpringBootApp extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(NotifyGatewaySpringBootApp.class, args);
-
     }
 
     @Override
@@ -30,4 +33,5 @@ public class NotifyGatewaySpringBootApp extends SpringBootServletInitializer {
         return (args) -> {
         };
     }
+
 }
